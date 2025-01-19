aca8acd7341e98fef743f9a8f659a2ad0ba937c6
Alessandro Sclafani <scla.alessandro@gmail.com>
2025-01-20 00:25:26 +0100

    modificato di conseguenza modalComponent, aggiunta logica sulla base degli interval. Ora viene mostrato anche un punteggio tra 1 e 100 come da consegna.

--- src/components/ModalComponent.jsx
+++ src/components/ModalComponent.jsx
@@ -1,5 +1,5 @@
 import {useImperativeHandle, useRef } from 'react';
-export default function ModalComponent({result, time, ref}){
+export default function ModalComponent({time, ref, timeRemaining, onReset}){
     // in questo caso, l'utilizzo di useRef in combinazione all'hook
     // imperativeHandle, serve a fare in modo che si crei un collegamento
     // fra i due useRef presenti qui e nel GameComponent. In questo modo,
@@ -8,7 +8,10 @@ export default function ModalComponent({result, time, ref}){
     // Se in futuro mi servirà modificare o aggiungere qualcosa alla funzione open, 
     // mi basterà modificarla da qui e verrà modificata anche in tutti i punti in cui
     // viene richiamata nel GameComponent.jsx
-    const displayModal = useRef()
+    const displayModal = useRef();
+    const score = Math.round((1 - ((timeRemaining / 1000) / time)) * 100)
+    const clickedIn = (time - (timeRemaining / 1000)).toFixed(2)
+    
     useImperativeHandle(ref, ()=>{
         return {
             open(){
@@ -18,10 +21,11 @@ export default function ModalComponent({result, time, ref}){
     })
     return(
         <dialog ref={displayModal} className="result-modal">
-            <h2>You lost</h2>
+            {timeRemaining <= 0 ? <h2>You lost</h2>: <h2>Your Score: {score}</h2>}
+            {timeRemaining > 0 && <p>You clicked it in {clickedIn}</p>}
             <p>You had to click it in {time} second{time > 1 && 's'}</p>
             <form method="dialog">
-                <button>Close</button>
+                <button onClick={onReset}>Close</button>
             </form>
         </dialog>
     )
