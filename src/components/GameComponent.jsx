d6c42a4e50afc1c3bda72bdbf9ef4de773273d98
Alessandro Sclafani <scla.alessandro@gmail.com>
2025-01-20 00:24:16 +0100

    Finita logica del gioco che ora viene gestita con setInterval invece di setTimeout. Creata funzione di reset passata poi tramite prop al ModalComponent.jsx che resetta lo state del TimeRemaining

--- src/components/GameComponent.jsx
+++ src/components/GameComponent.jsx
@@ -2,24 +2,31 @@ import {useRef, useState} from 'react';
 import ModalComponent from './ModalComponent.jsx'
 export default function GameComponent({difficulty, time }){
     const timer = useRef();
-    const [timerActive, setTimerActive] = useState(false);
     const displayModal = useRef();
+
+    const [timeRemaining, setTimeRemaining] = useState(time * 1000)
+    // timerActive verrà impostata a true se il tempo rimanente è maggiore di zero
+    // e se il tempo rimanente è minore della prop time*1000 (valore iniziale dello state)
+    const timerActive = timeRemaining > 0 && timeRemaining < time * 1000;
+    if(timeRemaining <= 0){
+        clearInterval(timer.current);
+        displayModal.current.open()
+    }
+    function manageReset(){
+        setTimeRemaining(time * 1000);
+    }
     function manageStart(){
-        setTimerActive(true);
-        timer.current = setTimeout(() => {
-            displayModal.current.open();
-            setTimerActive(false)
-        }, time * 1000);
-        
+        timer.current = setInterval(() => {
+            setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10);
+        }, 10);
     }
     function manageStop(){
-        clearTimeout(timer.current);
-        setTimerActive(false)
+        clearInterval(timer.current);
         displayModal.current.open();
     }
     return(
         <>
-            <ModalComponent ref={displayModal} time={time}/>
+            <ModalComponent onReset={manageReset} ref={displayModal} time={time} timeRemaining={timeRemaining}/>
             <div className="challenge">
                 <h2>{difficulty}</h2>
                 <p className="challenge-time">
