c0fef5b8d39672d7870096f8270b31693cc11642
Alessandro Sclafani <scla.alessandro@gmail.com>
2025-01-19 21:51:06 +0100

    Importato GameComponent e mappato in modo da evitare ripetizioni sul file precedentemente creato (gameDifficulties)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Player from './components/Player.jsx';
-
+import GameComponent from './components/GameComponent.jsx'
+import gameDifficulties from './gameDifficulties.js'
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        {gameDifficulties.map((singleGame)=> <GameComponent difficulty={singleGame.difficulty} time={singleGame.time}/>)}
+      </div>
     </>
   );
 }
