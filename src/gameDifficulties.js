b3161d548a98757988541cf6d7bed83fe329b414
Alessandro Sclafani <scla.alessandro@gmail.com>
2025-01-19 21:48:24 +0100

    creato file di utility per evitare ripetizIoni di components

---
+++ src/gameDifficulties.js
@@ -0,0 +1,19 @@
+const gameDifficulties = [ 
+    {
+      difficulty: 'Easy',
+      time: 1
+    },
+    {
+      difficulty: 'Medium',
+      time: 5
+    },
+    {
+      difficulty: 'Hard',
+      time: 10
+    },
+    {
+      difficulty: 'Pro',
+      time: 15
+    },
+  ] 
+  export default gameDifficulties
